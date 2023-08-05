4c6feb640aced8919534044cc7ea2778aa5174fb
Antonio Demarchi <56514877+DemarchiWorking@users.noreply.github.com>
2023-08-04 22:07:28 -0300

    feature:firestore

--- src/components/FormStep/NovaART/StepFour.jsx
+++ src/components/FormStep/NovaART/StepFour.jsx
@@ -1,6 +1,60 @@
+import { Button } from "react-bootstrap";
+import { db } from "../../../firebase";
+import { addDoc, collection, getDocs
+} from "firebase/firestore";
 
 
 const StepFour = () => {
+    const ref = collection(db, "Art")
+    
+    //const inputs = (e) => setFormulario({ ...formulario, [e.target.name]: e.target.value});
+    async function listarArt() {
+        console.log(ref);
+        const query = await collection(db, "Art");
+
+        const res = []
+        console.log(query);
+         //query.forEach((doc) => {  console.log(doc.id, " => ", doc.data());
+            //res.push(doc.data().toString());
+          
+            //setArts(doc.data());
+        //s})
+        //console.log(res);
+        //setArts(res);
+
+    
+    }
+    async function listarArte() {
+        try{
+            const data = await getDocs(ref);
+            const filteredData = data.docs.map((doc) => ({
+                ...doc.data(),
+                id: doc.id,
+            }));
+            console.log("en")
+            console.log(filteredData)
+        }catch(e){console.log(e)}
+    }
+    function adicionarArt(e){
+
+        // Bloquear o recarregamento da página
+        e.preventDefault();
+
+
+        try{
+            //DADO A SER INSERIDO NO BANCO DE DADOS - AQUI PRECISA SER ADICIONADO O OBJETO;
+            addDoc(ref, {"usuario:" : "test"});
+            console.log("ART:Enviado");
+//console.log("->"+ JSON.stringify(ref) + JSON.stringify(db));
+        }catch(error){
+            alert(error);
+        }
+    } 
+    return (
+        <div style={{ border: '1px solid #ccc', borderRadius: '5px', padding: '10px' }}><br></br><br></br>
+            <button onClick={adicionarArt} style={{backgroundColor:'blue',color:'white'}}> Enviar </button> 
+            <button onClick={listarArte} style={{backgroundColor:'blue',color:'white'}}> Listar </button>            
+        </div>)
 
 }
 export default StepFour
--- src/firebase.js
+++ src/firebase.js
@@ -3,21 +3,20 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBesJ436jaMt4CXYrVIUS4xu7lVdwkzsNE",
-    authDomain: "hackaton-crea.firebaseapp.com",
-    projectId: "hackaton-crea",
-    storageBucket: "hackaton-crea.appspot.com",
-    messagingSenderId: "450386955899",
-    appId: "1:450386955899:web:04cef9acdaa01bfaf59cde",
-    measurementId: "G-JKM586C24D"
-};
+    apiKey: "AIzaSyCTRom5QrMwursGKv75ykTEvFeKIc7EG2E",
+    authDomain: "hackathoncrea-468c4.firebaseapp.com",
+    projectId: "hackathoncrea-468c4",
+    storageBucket: "hackathoncrea-468c4.appspot.com",
+    messagingSenderId: "1088198864907",
+    appId: "1:1088198864907:web:2c2bdd23be17ac59c3d70e",
+    measurementId: "G-G5R3QZE37G"
+  };
 
-
-
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const firestore = getFirestore(app);
+export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider(); // Nova importação do GoogleAuthProvider
 export default app;
